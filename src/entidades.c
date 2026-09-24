cdf72de1c8afe8676ab3efeea178251ded4f5154
AnaCruz23 <anas1lv4cruz@gmail.com>
2026-09-24 15:59:24 -0300

    Adicionando comentários em entidades

--- src/entidades.c
+++ src/entidades.c
@@ -2,72 +2,73 @@
 #include "raylib.h"
 #include <stdio.h>
 #include <string.h>
-// ---------------------------------------------------------
-// RESPONSÁVEL PELAS ENTIDADES (PLAYER E INIMIGOS): [Ana Paula]
-// ---------------------------------------------------------
-// O que adicionar aqui:
-// 1. Criar a 'struct Posicao' com 'int x' e 'int y'. //Isso aqui não é necessário
-// 2. Criar a 'struct Jogador' contendo Nome (string), HP, Score e a 'struct Posicao' aninhada.
-// 3. Criar a 'struct Inimigo' contendo as mesmas coisas.
-// 4. Criar a função void moverJogador(Jogador *j, int matrizMapa[][]).
-// 5. Na função mover, ler as teclas WASD/Setas da Raylib (IsKeyPressed).
-// 6. Antes de alterar a posição x/y do jogador, verificar o IF na matriz do mapa 
-//    para impedir que ele ande por cima do número 1 (paredes).
 
 
 
+// Cria as características do jogador
 typedef struct{
-    char nome [10];
-    int hp;
-    int score;
-    Vector2 pos;
-    float tamanho;
-    float velocidade;
+    char nome [10]; 
+    int hp; // pontos de vida
+    int score; // pontuação atual do jogador
+    Vector2 pos; // posição (x,y) do jogador na tela 
+    float tamanho; // dimensão física do jogador (largura e altura do quadrado de colisão)
+    float velocidade; // quantidade de pixels que o jogador se move por frame
 } Jogador;
 
 
-
+// Cria as características do inimigo
 typedef struct{
     char nome [20];
-    int hp;
+    int hp; 
     float tamanho;
     Vector2 pos;
-    bool ativo; // define se está vivo ou morto
+    bool ativo; // flag booleana: true se o inimigo estiver vivo, false se for derrotado
 } Inimigo; 
 
+// Inicializar o jogador no jogo
+
 void inicializarJogador (Jogador *j, Vector2 posInicial){
-    strcpy (j-> nome, "Florzinha");
-    j-> hp=100;
+    strcpy (j-> nome, "Florzinha"); // Copia o texto "Florzinha" para o atributo nome do jogador
+    j-> hp=100; // vida inicial
     j -> score=0;
-    j -> tamanho = 30.0f;
-    j -> pos= posInicial;
-    j -> velocidade= 4.0f;
+    j -> tamanho = 30.0f; // define a largura e altura
+    j -> pos= posInicial; // define a posição inicial passada como parâmetro
+    j -> velocidade= 4.0f; // pixels por frame
 }
 
-void inicializarInimigo(Inimigo *i, Vector2 posInicial, const char *nome){
+// Inicializar o inimigo no jogo
+void inicializarInimigo(Inimigo *i, Vector2 posInicial){
     strcpy ( i -> nome, "Bug de C");
-    i -> hp=100;
-    i -> tamanho = 30.0f;
-    i -> pos = posInicial;
-    i -> ativo = true;
+    i -> hp=100; // vida do inimigo
+    i -> tamanho = 30.0f; // dimensão do inimigo
+    i -> pos = posInicial; // posição inicial do inimigo 
+    i -> ativo = true; // inicializa vivo
 }
+
+// Colocar o jogador para se mover com tratamento de colisão em dois eixos (AABB)
 
 void moverJogador(Jogador *j, int mapa[MAPA_LINHAS][MAPA_COLUNAS]){
-    float posXAnterior = j->pos.x;
+    // EIXO X
 
-    // Eixo X
+    float posXAnterior = j->pos.x; // Guarda a posição X antes de mover
+
+    // Verifica se as teclas da direita e a D estão posicionadas para mover para a direita
     if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) j->pos.x += j->velocidade;
+    // Verifica se as teclas da esquerda e a A estão  pressionadas para mover para a esquerda
     if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))  j->pos.x -= j->velocidade;
-
+    // Cria um retângulo de colisão com a nova posição do eixo X
     Rectangle recX = { j->pos.x, j->pos.y, j->tamanho, j->tamanho };
+    // Se a nova posição colidir com algum obstáculo do mapa, restaura a posição X anterior
     if (checarColisaoComMapa(mapa, recX)) j->pos.x = posXAnterior;
+    // EIXO Y
+    float posYAnterior = j->pos.y; // Guarda a posição Y antes de mover
 
-    float posYAnterior = j->pos.y;
-
-    // Eixo Y
+    // Verifica se as teclas 'S' ou 'Seta para Baixo' estão pressionadas para mover para baixo
     if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) j->pos.y += j->velocidade;
+    // Verifica se as teclas 'W' ou 'Seta para Cima' estão pressionadas para mover para cima
     if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))   j->pos.y -= j->velocidade;
-
+    // Cria um retângulo de colisão com a nova posição no eixo Y (já validada no eixo X)
     Rectangle recY = { j->pos.x, j->pos.y, j->tamanho, j->tamanho };
+    // Se a nova posição colidir com o mapa, restaura a posição Y anterior
     if (checarColisaoComMapa(mapa, recY)) j->pos.y = posYAnterior;
 }
